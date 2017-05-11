30c13f3b3374a2cf5d439f6e8498943785fbd153
Yang <zhiyangw@stanford.edu>
2017-05-11 10:38:39 -0700

    model that works on random permeability with affine transform of pressure

--- test_model.py
+++ test_model.py
@@ -4,15 +4,16 @@ import random
 from model import Model
 
 class Config():
-    nblocks = 10
+    nx = 10
     hidden_size = 20
     lr = 0.001
-    n_epochs =3
+    n_epochs =10
     batch_size = 2
+    conv_size =3
 
 
 class TestModel(Model):
-   def add_placeholders(self):
+    def add_placeholders(self):
         """Adds placeholder variables to tensorflow computational graph.
         Tensorflow uses placeholder variables to represent locations in a
         computational graph where data is inserted.  These placeholders are used as
@@ -22,9 +23,8 @@ class TestModel(Model):
         https://www.tensorflow.org/versions/r0.7/api_docs/python/io_ops.html#placeholders
         """
 
-        self.input_placeholder   = tf.placeholder(tf.float32,   (None, self.config.nblocks), name = "input")
-        self.labels_placeholder  = tf.placeholder(tf.float32,   (None, self.config.nblocks), name = "out")
-
+        self.input_placeholder   = tf.placeholder(tf.float32,   (None, self.config.nx,self.config.nx,1), name = "input")
+        self.labels_placeholder  = tf.placeholder(tf.float32,   (None, self.config.nx,self.config.nx,1), name = "out")
     def create_feed_dict(self, inputs_batch, labels_batch=None):
         """Creates the feed_dict for one step of training.
         A feed_dict takes the form of:
@@ -60,8 +60,10 @@ class TestModel(Model):
             pred: A tensor of shape (batch_size, n_classes)
         """
         config = self.config
-        W   = tf.get_variable('W',  shape = [config.hidden_size, config.nblocks], initializer = tf.contrib.layers.xavier_initializer())
-        preds = tf.matmul(W,self.input_placeholder)
+        conv1 = tf.layers.conv2d(inputs=self.input_placeholder, filters=32,kernel_size=[config.conv_size, config.conv_size], padding="same",activation=tf.nn.relu)
+        preds = tf.reduce_sum(conv1,axis = -1,keep_dims= True)
+
+        return preds
 
 
     def add_loss_op(self, pred):
@@ -71,7 +73,8 @@ class TestModel(Model):
         Returns:
             loss: A 0-d tensor (scalar) output
         """
-        loss = tf.norm(preds-self.labels_placeholder)
+        loss = tf.norm(pred-self.labels_placeholder)
+        return loss
 
     def add_training_op(self, loss):
         """Sets up the training Ops.
@@ -114,28 +117,34 @@ class TestModel(Model):
         return predictions
 
     def fit(self,sess,train_examples,dev_set):
-        best_dev = 0
+        best_dev = 1e9
         for epoch in range(self.config.n_epochs):
             print ("Epoch {:} out of {:}".format(epoch + 1, self.config.n_epochs))
             dev_score = self.run_epoch(sess,train_examples,dev_set)
-            if dev_score > best_dev:
-                best_dev = dev_score:
-                print("new best score found {:}".format(best_dev))
-    def run_epoch(sess,train_examples,dev_set):
+            if dev_score < best_dev:
+                best_dev = dev_score
+                print("new best norm found {:}".format(best_dev))
+    def run_epoch(self,sess,train_examples,dev_set):
+        config = self.config
         num_train = len(train_examples)
-        ind = range(0,num_train)
-        ind = random.shuffle(ind)
+        ind = np.arange(num_train)
+        np.random.shuffle(ind)
+        ind.astype(int)
+        batchNum = 1
+        num_batch = int(np.ceil(num_train/config.batch_size))
         for i in range(0, num_train,config.batch_size):
-            batch_train = train_examples[ind[i:i+config.batch_size]]
+            batch_train = [ train_examples[i] for i in ind[i:i+config.batch_size]]
             input_train , labels_train =  zip(*batch_train)
-            self.train_on_batch(sess,input_train,labels_train)
-        input_dev , labels_dev =  zip(*batch_train)
-        pred = self.predict_on_batch(sess,input_dev)
-        print("Predicted pressure")
-        print(pred)
-        print("Development pressure")
-        print(labels_dev)
+            loss = self.train_on_batch(sess,input_train,labels_train)
+            print("Loss for Batch {:} out of {:} is: {:}".format(batchNum,num_batch,loss))
+            batchNum += 1
 
+        input_dev , labels_dev =  zip(*dev_set)
+        pred = self.predict_on_batch(sess,input_dev)
+        norms = []
+        for i in range(len(labels_dev)):
+            norms.append(np.linalg.norm(labels_dev[i]-pred[i,:,:,:]))
+        return np.max(norms)
 
 
     def build(self):
@@ -152,17 +161,17 @@ if __name__ == "__main__":
     config = Config()
     # create random permeability
     n_train = 15
-    train_perm = np.random.random((config.nblocks,n_train))
+    train_perm = np.random.random((n_train,config.nx,config.nx,1))
     train_pres = 5*train_perm - 2 
     train_examples = []
     for i in range(n_train):
-        train_examples.push_back(train_perm[:,i],train_pres[:,i])
+        train_examples.append((train_perm[i,:,:,:],train_pres[i,:,:,:]))
     n_dev = 3
-    dev_perm = np.random.random((config.nblocks,n_dev))
+    dev_perm = np.random.random((n_dev,config.nx,config.nx,1))
     dev_pres = 5*train_perm - 2 
     dev_set = []
     for i in range(n_dev):
-        dev_set.push_back(dev_perm[:,i],dev_pres[:,i])
+        dev_set.append((dev_perm[i,:,:,:],dev_pres[i,:,:,:]))
 
 
     with tf.Graph().as_default():
@@ -170,6 +179,5 @@ if __name__ == "__main__":
         model = TestModel(config)
         init = tf.global_variables_initializer()
         with tf.Session() as session:
-            parser.session = session
             session.run(init)
-            model.fit(session, saver, parser, train_examples, dev_set)
+            model.fit(session, train_examples, dev_set)
