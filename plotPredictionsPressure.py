5eef4d62a6a04880cfc4a4f348c65c862046eb51
Yang-instance3 <zhiyangw@stanford.edu>
2017-05-26 19:49:41 +0000

    added pressure model that works okayish, needs more data and work still

--- plotPredictionsPressure.py
+++ plotPredictionsPressure.py
@@ -1,13 +1,12 @@
 import numpy as np
 import os as os
+import matplotlib
+matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 import pickle as pickle
 from matplotlib import gridspec
-import matplotlib.pyplot as plt
 import matplotlib.colors as colors
 from deepres.simulator.periodic_field_functions import PeriodicPerturbations
-
-
 proj_folder = os.path.dirname(os.path.realpath(__file__))
 # path to save the training data file
 save_folder = os.path.join(proj_folder, 'temp')
@@ -17,14 +16,15 @@ grid_path = os.path.join(proj_folder, 'sample_scripts', 'script_files', '100_100
 #data = np.load(save_path)
 #X = data['X']
 #Y = data['Y']
+#datFile =np.load('train_pressure.npz')
 
 datFile =np.load('best_pred_pressure.npz')
 
 X = datFile['perm']
-pressure_actual = datFile['y_pressure']
+pressure_actual = datFile['pressure']
 p_pred = datFile['best_pred']
 
-
+print(p_pred.shape)
 with open(grid_path, 'rb') as input:
     grid = pickle.load(input)
 dx, dy = grid.dx, grid.dy
@@ -36,68 +36,7 @@ y_mat = np.reshape(gridy, (nx,nx), order='F')
 # plot permeability
 
 PI = PeriodicPerturbations(grid, None, None)
-for i in range(5):
-	fig = plt.figure()
-	ax = fig.add_subplot(1,2,1)
-	perm_mat = X[i,:,:,0]
-	p = ax.pcolormesh(x_mat, y_mat, perm_mat, cmap=plt.cm.coolwarm)
-	ax.set_aspect('equal', 'box')
-	u_cell, v_cell = PI.get_cell_velocity(u_actual[i,:])
-	u_actual_mat = np.reshape(u_cell, (nx,nx), order='F')
-	v_actual_mat = np.reshape(v_cell, (nx,nx), order='F')
-	f = 12
-	ax.quiver(gridx[::f], gridy[::f], u_cell[::f], v_cell[::f], units='inches')
-
-	# f = 12
-	# ax.quiver(gridx[::f], gridy[::f], u[::f], v[::f], units='inches')
-	# ax.streamplot(x_mat, y_mat, u_mat, v_mat, linewidth=1.0, color='w')
-	# ax.quiver(gridx, gridy, u, v, units='width')
-	ax.set_ybound([0,nx])
-	ax.set_xbound([0,nx])
-	ax.set_aspect('equal', 'box')
-	cbar = fig.colorbar(p, fraction=0.046, pad=0.04)
-
-	ax = fig.add_subplot(1,2,2)
-	perm_mat = X[i,:,:,0]
-	p = ax.pcolormesh(x_mat, y_mat, perm_mat, cmap=plt.cm.coolwarm)
-	ax.set_aspect('equal', 'box')
-	u_cell, v_cell = PI.get_cell_velocity(u_pred[i,:])
-	u_pred_mat = np.reshape(u_cell, (nx,nx), order='F')
-	v_pred_mat = np.reshape(v_cell, (nx,nx), order='F')
-	f = 12
-	ax.quiver(gridx[::f], gridy[::f], u_cell[::f], v_cell[::f], units='inches')
-	# f = 12
-	# ax.quiver(gridx[::f], gridy[::f], u[::f], v[::f], units='inches')
-	# ax.streamplot(x_mat, y_mat, u_mat, v_mat, linewidth=1.0, color='w')
-	# ax.quiver(gridx, gridy, u, v, units='width')
-	ax.set_ybound([0,nx])
-	ax.set_xbound([0,nx])
-	ax.set_aspect('equal', 'box')
-	cbar = fig.colorbar(p, fraction=0.046, pad=0.04)
-
-	fig.savefig(os.path.join(save_folder, 'perm{:}.png'.format(i)), format='png')
-
-	fig = plt.figure(figsize=(9,4))
-
-	gs = gridspec.GridSpec(1, 2)
-	gs.update(left=0.1, right=0.86, hspace=0.1)
-	gs1 = gridspec.GridSpec(1, 1)
-	gs1.update(left=0.89, right=0.92)
-	ax = plt.subplot(gs[0])
-	ax2 = plt.subplot(gs[1])
-	ax_cmap = plt.subplot(gs1[0])
-	
-	cmap = plt.cm.afmhot_r
-	
-	p = ax.pcolormesh(x_mat, y_mat, u_pred_mat, cmap=plt.cm.coolwarm,vmin = 0,vmax =0.5 )
-
-	ax.set_aspect('equal', 'box')
-
-	p2 = ax2.pcolormesh(x_mat, y_mat, u_actual_mat, cmap=plt.cm.coolwarm ,vmin = 0, vmax = 0.5)
-	cbar = fig.colorbar(p, ax_cmap, orientation='vertical')
-	
-	ax.set_aspect('equal', 'box')
-	fig.savefig(os.path.join(save_folder, 'u_both{:}.png'.format(i)), format='png')
+for i in range(10):
 	
 	fig = plt.figure(figsize=(9,4))
 
@@ -111,15 +50,15 @@ for i in range(5):
 	
 	cmap = plt.cm.afmhot_r
 	
-	p = ax.pcolormesh(x_mat, y_mat, v_pred_mat, cmap=plt.cm.coolwarm,vmin = -0.15,vmax =0.15 )
+	p = ax.pcolormesh(x_mat, y_mat, p_pred[i,:,:,0], cmap=plt.cm.coolwarm,vmin = -0.75,vmax =0.75 )
 
 	ax.set_aspect('equal', 'box')
 
-	p2 = ax2.pcolormesh(x_mat, y_mat, v_actual_mat, cmap=plt.cm.coolwarm ,vmin = -0.15, vmax = 0.15)
+	p2 = ax2.pcolormesh(x_mat, y_mat, pressure_actual[i,:,:,0], cmap=plt.cm.coolwarm ,vmin = -0.75, vmax = 0.75)
 	cbar = fig.colorbar(p2, ax_cmap, orientation='vertical')
 	
 	ax.set_aspect('equal', 'box')
-	fig.savefig(os.path.join(save_folder, 'v_both{:}.png'.format(i)), format='png')
+	fig.savefig(os.path.join(save_folder, 'pressure_both{:}.png'.format(i)), format='png')
 	
 	# def compare_trans_mat_vtheta(trans_matrix_1, trans_matrix_2, figure_save_folder, prefix, refsize = 16):
 
