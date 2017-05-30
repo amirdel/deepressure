3092fa537ad445d2dddd1cb11633939cafaf6378
amirdel <amirdel@stanford.edu>
2017-05-30 09:37:31 -0700

    generating nonperiodic pressure fields

--- deepres/simulator/generate_training_data.py
+++ deepres/simulator/generate_training_data.py
@@ -16,7 +16,7 @@ import pandas as pd
 from deepres.simulator.linear_system_solver import LinearSystemStandard
 from deepres.simulator.periodic_field_functions import PeriodicPerturbations
 
-def generate_continuum_realizations(grid_path, save_path, perm_path, dp_x, dp_y, n_images, print_every=50):
+def generate_continuum_realizations_periodic(grid_path, save_path, perm_path, dp_x, dp_y, n_images, print_every=50):
     """
     This function will create the training data for our model
     :param grid_path: full path to a grid object (../grid.pkl)
@@ -67,7 +67,6 @@ def generate_continuum_realizations(grid_path, save_path, perm_path, dp_x, dp_y,
         # copy the pressure solution and the permeability field to the X and Y
         X[i, :, :, 0] = np.reshape(logperm, (n_cell, n_cell))
         Y[i, :, :, 0] = np.copy(np.reshape(LS.sol, (n_cell, n_cell)))
-        # perform particle tracking
         grid.pressure = LS.sol
         # get the operators to calculate face velocity
         U_face_operator, U_face_fixed = PI.face_velocity_operator(grid.transmissibility)
@@ -78,3 +77,67 @@ def generate_continuum_realizations(grid_path, save_path, perm_path, dp_x, dp_y,
         face_bias_array[i,:] = U_face_fixed
     # save X, Y, U_face, operators
     np.savez(save_path, X=X, Y=Y, U_face=U_face, U_face_operator=face_operator_list, U_face_fixed=face_bias_array)
+
+
+def generate_continuum_realizations_nonperiodic(grid_path, save_path, perm_path, n_images, print_every=50):
+    """
+    This function will create the training data for our model
+    :param grid_path: full path to a grid object (../grid.pkl)
+    :param save_path: full path for saveing the (../*.npz)
+    :param perm_path: full path to permeability realiztions (.../*.csv)
+    :param dp_x: mean pressure gradient in the x direction
+    :param dp_y: mean pressure gradient in the y direction
+    :param n_images: number of images/training data
+    :param print_every: print every for output messages
+    :return:
+    """
+    # loading the grid
+    with open(grid_path, 'rb') as input:
+        grid = pickle.load(input)
+    n_cell = grid.m
+    n_face = grid.nr_t
+    # find the indices for the left and right boundary
+    cell_x = grid.pores.x
+    x_min, x_max = np.amin(cell_x), np.amax(cell_x)
+    idxmin = np.where(cell_x == x_min)[0]
+    idxmax = np.where(cell_x == x_max)[0]
+    # initialize perm matrix and pressure solution (n_cell x n_cell x n_perm_fields)
+    X, Y = [np.zeros((n_images, n_cell, n_cell, 1)) for i in range(2)]
+    # initialize arrays for saving the faces velocities
+    U_face = np.zeros((n_images, n_face))
+    # initialize the array for saving the face operator and bias
+    face_operator_list = []
+    face_bias_array = np.zeros((n_images, n_face))
+    # load the permeability dataframe, each column is one realization
+    # this is the file saved by SGEMS (Geostats software)
+    perm_frame = pd.read_csv(perm_path, usecols=range(n_images))
+    # initialize a linear system for the pressure fluctuations for the grid
+    LS = LinearSystemStandard(grid)
+    # for the number of specified realizations run particle tracking and save the results
+    for i in range(n_images):
+        if not i%print_every:
+            print('realization number '+str(i))
+        logperm = perm_frame.ix[:, i]
+        perm = np.exp(logperm)
+        grid.set_transmissibility(perm)
+        # setting the left hand side of the equation
+        LS.fill_matrix(grid.transmissibility)
+        # set dirichlet BC
+        LS.set_dirichlet_pores(idxmin, 1.0)
+        LS.set_dirichlet_pores(idxmax, 0.0)
+        # solve for pressure
+        LS.solve()
+        # copy the pressure solution and the permeability field to the X and Y
+        X[i, :, :, 0] = np.reshape(logperm, (n_cell, n_cell))
+        Y[i, :, :, 0] = np.copy(np.reshape(LS.sol, (n_cell, n_cell)))
+        grid.pressure = LS.sol
+        # get the operators to calculate face velocity
+        # U_face_operator = face_velocity_operator(grid.transmissibility)
+        U_face_operator = None
+        # save face_velocity
+        # U_face[i,:] = U_face_operator.dot(LS.sol)
+        U_face[i, :] = np.zeros(n_face)
+        # save the face operator
+        face_operator_list.append(U_face_operator)
+    # save X, Y, U_face, operators
+    np.savez(save_path, X=X, Y=Y, U_face=U_face, U_face_operator=face_operator_list)
