1396f1747b9731561a57101cc86ebc98e8d9cd33
amirdel <amirdel@stanford.edu>
2017-05-30 17:37:26 -0700

    adding divergence operator

--- deepres/simulator/operators.py
+++ deepres/simulator/operators.py
@@ -1,4 +1,5 @@
 from scipy.sparse import coo_matrix
+import numpy as np
 
 def face_velocity_operator_nonperiodic(grid):
     transRockGeometric = grid.transmissibility
@@ -7,7 +8,6 @@ def face_velocity_operator_nonperiodic(grid):
     dx, dy, dz = grid.dx, grid.dy, grid.dz
     y_faces, d_cell_numbers = grid.y_faces, grid.d_cell_numbers
     face_adj_list = grid.updwn_cells
-    b_counter = 0
     for face in range(grid.nr_t):
         # find adjacent cells
         adj_cells = face_adj_list[face]
@@ -28,7 +28,6 @@ def face_velocity_operator_nonperiodic(grid):
         col.append(dwn)
         data.append(-trans / A)
     face_vel_operator = coo_matrix((data, (row, col)), shape=(grid.nr_t, grid.nr_p))
-    print('number of boundary cells: ', b_counter)
     return face_vel_operator
 
 def get_cell_velocity(grid, face_velocities):
@@ -36,3 +35,19 @@ def get_cell_velocity(grid, face_velocities):
     u = 0.5 * (face_velocities[cell_faces[:, 0]] + face_velocities[cell_faces[:, 1]])
     v = 0.5 * (face_velocities[cell_faces[:, 2]] + face_velocities[cell_faces[:, 3]])
     return u,v
+
+def divergence_operator(grid):
+    row, col, data  = [np.empty([0]) for i in range(3)]
+    # order of ngh_faces : left, right, bottom , top, -1 if missing
+    ngh_faces = grid.facelist_array
+    n_cells = grid.nr_p
+    for cell in range(n_cells):
+        cell_faces = ngh_faces[cell]
+        mask = (cell_faces != -1)
+        sgn = np.array([-1.0, 1.0, -1.0, 1.0])
+        current_val = sgn[mask]
+        current_row = cell* np.ones(4, dtype=np.int)
+        row = np.hstack((row, current_row[mask]))
+        col = np.hstack((col, cell_faces[mask]))
+        data = np.hstack((data, current_val))
+    return coo_matrix((data[1:], (row[1:], col[1:])), shape=(grid.nr_p, grid.nr_t))
--- sample_scripts/plot_fields_nonperiodic.py
+++ sample_scripts/plot_fields_nonperiodic.py
@@ -4,7 +4,7 @@ import os as os
 import matplotlib.pyplot as plt
 import pickle as pickle
 # from deepres.simulator.periodic_field_functions import PeriodicPerturbations
-from deepres.simulator.operators import face_velocity_operator_nonperiodic, get_cell_velocity
+from deepres.simulator.operators import face_velocity_operator_nonperiodic, get_cell_velocity, divergence_operator
 
 proj_folder = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 print(proj_folder)
@@ -26,6 +26,8 @@ face_vel_operator = data['U_face_operator']
 
 with open(grid_path, 'rb') as input:
     grid = pickle.load(input)
+# get the divergence operator
+div_operator = divergence_operator(grid)
 n_cells = grid.nr_p
 dx, dy = grid.dx, grid.dy
 nx = grid.m
@@ -36,17 +38,21 @@ gridy = grid.pores.y
 x_mat = np.reshape(gridx, (nx,nx))
 y_mat = np.reshape(gridy, (nx,nx))
 # plot permeability
-for i in range(5):
+for i in range(1):
     fig = plt.figure()
     ax = fig.add_subplot(1,1,1)
     perm_mat = X[i,:,:,0]
     p = ax.pcolormesh(x_mat, y_mat, perm_mat, cmap=plt.cm.coolwarm)
     # ax.set_aspect('equal', 'box')
     # plot the cell center velocity on top of perm
-    div = face_vel_operator[i]
+    face_v_operator = face_vel_operator[i]
     pressure_vec = Y[i,:,:,0].reshape((n_cells))
     # u_bias = face_vel_bias[i,:]
-    u_face = div.dot(pressure_vec) #+ u_bias
+    u_face = face_v_operator.dot(pressure_vec) #+ u_bias
+    print('mean velocity: ', np.mean(u_face))
+    # calculate the divergence
+    div = div_operator.dot(u_face)
+    print('mean divergence: ', np.mean(div))
     # get cell center velocity from face velocity
     u_cell, v_cell = get_cell_velocity(grid, u_face)
     # u_mat = np.reshape(u_cell, (nx,nx), order='F')
@@ -68,3 +74,9 @@ for i in range(5):
     cbar = fig.colorbar(p, fraction=0.046, pad=0.04)
     ax.set_aspect('equal', 'box')
     fig.savefig(os.path.join(save_folder, 'pressure'+str(i)+'.png'), format='png')
+    # plot the divergence
+    fig, ax = plt.subplots(1,1)
+    p = ax.pcolormesh(x_mat, y_mat, np.reshape(div, (nx,nx)), cmap=plt.cm.coolwarm)
+    cbar = fig.colorbar(p, fraction=0.046, pad=0.04)
+    ax.set_aspect('equal', 'box')
+    fig.savefig(os.path.join(save_folder, 'divergence' + str(i) + '.png'), format='png')
